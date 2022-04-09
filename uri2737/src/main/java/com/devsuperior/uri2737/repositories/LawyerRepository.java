a07df41f5b8e4543071ebe792c0164870311775e
Rafaelxndre <rafaelxndre@gmail.com>
2022-04-09 16:57:42 -0300

    uri2737 - Spring Boot SQL

--- uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.uri2737.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.uri2737.entities.Lawyer;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
 
+	@Query(nativeQuery = true, value = "(select name,customers_number as customersNumber from lawyers where customers_number = (select MIN(customers_number) from lawyers )) "
+			+ "union all "
+			+ "(select name,customers_number from lawyers where customers_number = (select MAX(customers_number) from lawyers )) "
+			+ "union all "
+			+ "(select 'Avarage', ROUND(AVG(customers_number),0) FROM lawyers)")
+	List<LawyerMinProjection> search1();
 }
